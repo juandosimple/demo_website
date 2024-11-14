bd33c103b27c848964a9601f21db87cb55287137
Juando <sieben.co@gmail.com>
2024-11-14 11:53:28 -0300

    A

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,23 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+
+
+// TITLE ANIMATION
+const texts = document.querySelectorAll(".animated-text");
+let index = 0;
+
+function changeText() {
+  if (index > 0) {
+    texts[index - 1].classList.remove("show"); // Oculta la palabra anterior
+  }
+  
+  texts[index].classList.add("show"); // Muestra la palabra actual
+  index++;
+
+  if (index < texts.length) {
+    setTimeout(changeText, 2000); // Cambia cada 2 segundos
+  }
+}
+
+setTimeout(changeText, 000); // Espera antes de iniciar el primer cambio
